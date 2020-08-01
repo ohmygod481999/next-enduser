f124c16167788eb70c4025d8362c602e092288ce
longvb <vuongbaolong48@gmail.com>
2020-08-01 12:05:18 +0700

    update elastic search

--- utils/elasticsearch.js
+++ utils/elasticsearch.js
@@ -7,15 +7,49 @@ class ElasticClient {
             password: "dSPKMcdQkG5X97b",
         };
         const root = "https://es.foodizzi.com";
-        this.merchantInstance = new Client({
-            node: root + "/merchants/merchant",
+        this.eInstance = new Client({
+            node: root,
             auth: auth,
         });
-        this.categoryInstance = new Client({
-            node: root + "/categories/category",
-            auth: auth
-        })
     }
+
+    search = ({ index, type, body, from, size }) => {
+        return this.eInstance.search({
+            index: index,
+            type: type,
+            body: body,
+        });
+    };
+
+    getCategories = ({ body, from, size }) => {
+        return this.search({
+            index: "categories",
+            type: "category",
+            body: body,
+            from: from,
+            size: size,
+        });
+    };
+
+    getMerchant = ({ merchantCode = "cokoglobal", from, size = 1 }) => {
+        return this.search({
+            index: "merchants",
+            type: "merchant",
+            body: {
+                query: {
+                    match: {
+                        code: {
+                            query: merchantCode,
+                        },
+                    },
+                },
+                sort: [],
+                _source: {},
+            },
+            from: from,
+            size: size,
+        });
+    };
 }
 
 const eInstance = new ElasticClient();
